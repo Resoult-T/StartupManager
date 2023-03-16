5dee89e75aca7814c864792a57b5ea52f963428a
Tom <102725449+Resoult-T@users.noreply.github.com>
2023-03-16 22:24:11 +0100

    Added significantly faster way to get ParentPRocessId's resolves #7 - Using the NtQueryInformationProcess() from ntdll.dll instead of WMI

--- StartupManager/ProcessHelper.cs
+++ StartupManager/ProcessHelper.cs
@@ -6,6 +6,7 @@ using System.Drawing.Printing;
 using System.Linq;
 using System.Management;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,30 +16,45 @@ namespace StartupManager
 {
     public static class ProcessHelper
     {
+
+        [DllImport("ntdll.dll")]
+        private static extern int NtQueryInformationProcess(IntPtr hProcess, int processInformationClass, ref PROCESS_BASIC_INFORMATION processBasicInfo, int processInformationLength, out int returnLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+
+        // The only instance of a list with all processes
         private static Process[] allProcesses;
 
+        /// <summary>
+        /// Finds the mainWindowHandle of the passed process
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="settings"></param>
+        /// <returns>mainWindowHandle</returns>
         public static IntPtr FindMainWindowHandle(Process process, ref ExecutableSettings settings)
         {
             // Set this process to the root proces
             var root = Process.GetCurrentProcess();
 
-            // Later when processes are started in paralell it must be found out to which thread which mainWindowHandle belongs to
-            var thread = Thread.CurrentThread;
-
-
+            // Return value
             var handle = IntPtr.Zero;
 
             // Timeout after which it will return a zero ptr
-            var timeout = DateTime.UtcNow.AddSeconds(10);
+            var timeout = DateTime.UtcNow.AddSeconds(20);
 
-            var foundWindowHandles = new List<IntPtr>(); 
+            // This list contains all found handles that should be ignored during the next run
+            var foundWindowHandles = new List<IntPtr>();
 
-
-
-            // Get the mainWindowHandle of the process 
+            // Search for handle until found or timeout is triggered
             while (handle == IntPtr.Zero || foundWindowHandles.Count <= settings.SkipAmountOfWindows && DateTime.UtcNow < timeout)
             {
-                
+                // Initialize the processes only once per iteration
                 allProcesses = Process.GetProcesses();
                 
                 
@@ -47,6 +63,7 @@ namespace StartupManager
                     // Root process is no longer running
                     break;
                 }
+                
                 
                 var childProcesses = GetChildProcesses(root);
 
@@ -65,30 +82,40 @@ namespace StartupManager
                         break;
                     }
                 }
+
+                if (handle == IntPtr.Zero)
+                {
+                    Thread.Sleep(100);
+                }
             }
 
             return handle;
         }
 
         /// <summary>
-        /// Searches for all child processes of the root process
+        /// Searches for all child processes of the root process.
         /// </summary>
         /// <param name="root">The Parent proces</param>
-        /// <returns>Alls child processes related to the given parent process</returns>
+        /// <returns>Alls child processes related to the given parent process.</returns>
         private static Process[] GetChildProcesses(Process root)
         {
+
             // Dictionary that stores a PerentProcess with key ChildProcess
             ConcurrentDictionary<int, int> parentProcessIds = new ConcurrentDictionary<int, int>();
             // Contains all found child proceses related to the root Process
             Process[] childProcesses = new Process[allProcesses.Length];
 
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             // Store the parent process ID for each process ID in a dictionary
             Parallel.ForEach(allProcesses, process =>
             {
                 parentProcessIds[process.Id] = process.ParentProcessId();
-                Console.WriteLine(process.ProcessName);
             });
 
+            stopwatch.Stop();
+            Console.WriteLine("Time: {0}", stopwatch.ElapsedMilliseconds);
 
             // Use a breadth-first search algorithm to find the child processes of the root process
 
@@ -124,41 +151,38 @@ namespace StartupManager
             return childProcesses;
         }
 
+
         /// <summary>
-        /// Gets the Perent of the current process
+        /// Gets the process ID of the parent process of a child
         /// </summary>
-        /// <param name="process"></param>
-        /// <returns></returns>
-        private static Process Parent(this Process process)
-        {
-            int parentProcessId = process?.ParentProcessId() ?? -1;
-            if (parentProcessId <= 0)
-            {
-                return null;
-            }
-
-            foreach (Process p in allProcesses)
-            {
-                if (p.Id == parentProcessId)
-                {
-                    return p;
-                }
-            }
-
-            return null;
-        }
-
-
-        // TODO: Find a methd that is faster than this one 
+        /// <param name="process">The child process</param>
+        /// <returns>ParentProcessId</returns>
         public static int ParentProcessId(this Process process)
         {
-            int parentProcessId;
+            // Return value
+            int parentProcessId = 0;
             try
             {
-                using (ManagementObject managementObject = new ManagementObject($"win32_process.handle='{process.Id}'"))
+                PROCESS_BASIC_INFORMATION processBasicInfo = new PROCESS_BASIC_INFORMATION();
+                int processBasicInfoSize = Marshal.SizeOf(processBasicInfo);
+                int bytesReturned = 0;
+
+                // Get the handle of the child's proces
+                IntPtr hProcess = OpenProcess(ProcessAccessFlags.QueryInformation, false, process.Id);
+                if (hProcess != IntPtr.Zero)
                 {
-                    managementObject.Get();
-                    return Convert.ToInt32(managementObject["ParentProcessId"]);
+                    try
+                    {
+                        // Fill the processBasicInfo Struct with data
+                        NtQueryInformationProcess(hProcess, 0, ref processBasicInfo, processBasicInfoSize, out bytesReturned);
+
+                        // Get the ParentProcesId from processBasicInfo
+                        parentProcessId = processBasicInfo.InheritedFromUniqueProcessId.ToInt32();
+                    }
+                    finally
+                    {
+                        CloseHandle(hProcess);
+                    }
                 }
             }
             catch (Exception)
@@ -166,6 +190,25 @@ namespace StartupManager
                 parentProcessId = 0;
             }
             return parentProcessId;
+        }
+
+        // Buffer for the process basic information
+        [StructLayout(LayoutKind.Sequential)]
+        private struct PROCESS_BASIC_INFORMATION
+        {
+            public IntPtr Reserved1;
+            public IntPtr PebBaseAddress;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public IntPtr[] Reserved2;
+            public IntPtr UniqueProcessId;
+            public IntPtr InheritedFromUniqueProcessId;
+        }
+
+        // Flags for OpenProcess()
+        [Flags]
+        private enum ProcessAccessFlags : uint
+        {
+            QueryInformation = 0x0400
         }
     }
 }
